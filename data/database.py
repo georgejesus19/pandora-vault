ef27b318d885828e18f0d1a4fe06e80314fbe9e4
George <georgehebo18@gmail.com>
2026-09-22 22:45:35 +0100

    Feature: Add entries creation feature and path usability improvement

--- data/database.py
+++ data/database.py
@@ -1,5 +1,9 @@
 import os
 import sqlite3
+import datetime
+
+base_path = os.path.dirname(os.path.abspath(__file__))
+VAULT_DB_PATH = os.path.join(base_path, "vault.db")
 
 def open_connection(path):
     try:
@@ -16,11 +20,34 @@ def close_connection(connection):
         except Exception:
             pass
 
+def create_entry(location, username, ciphertext, nonce, authentication_tag,path=""):
+    connection = None
+    created_at = updated_at = datetime.datetime.now().replace(microsecond=0).isoformat()
+    try:
+        query = """
+                INSERT INTO entries (location, username, ciphertext, 
+                nonce, authentication_tag, created_at, updated_at)
+                VALUES (?, ?, ?, ?, ?, ?, ?)
+                 """
+        if path.strip() == "":
+            connection = open_connection(VAULT_DB_PATH)
+        else:
+            connection = open_connection(path)
+        if not connection:
+            return False
+        cursor = connection.cursor()
+        cursor.execute(query, (location, username, ciphertext,
+                               nonce, authentication_tag, created_at, updated_at))
+        connection.commit()
+        return True
+    except Exception:
+        return False
+    finally:
+        close_connection(connection)
+
 def initialize_database(path=""):
     if path.strip() == "":
-        base_path = os.path.dirname(os.path.abspath(__file__))
-        db_path = os.path.join(base_path, "vault.db")
-        connection = open_connection(db_path)
+        connection = open_connection(VAULT_DB_PATH)
     else:
         connection = open_connection(path)
     try:
