5da8df251f829e69feed6b36b169240cc7dce761
George <georgehebo18@gmail.com>
2026-09-22 22:46:29 +0100

    Feature: Entries creation tests implementation with pytest

--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,14 @@
+import secrets
+import pytest
 from data import database
+import src.crypto.kfd as kfd
+import src.crypto.encryption as encryption
 
+DEFAULT_MEMORY_COST = kfd.DEFAULT_MEMORY_COST
+DEFAULT_TIME_COST = kfd.DEFAULT_TIME_COST
+DEFAULT_PARALLELISM = kfd.DEFAULT_PARALLELISM
+SALT_LENGTH = kfd.SALT_LENGTH
+NONCE_LENGTH = encryption.NONCE_LENGTH
 
 def test_database_creation():
     database_creation = database.initialize_database("test_file/test_database.db")
@@ -16,3 +25,51 @@ def test_database_entries():
 
     columns_name = [line["name"] for line in info]
     assert expected_colunms == columns_name
+
+def test_create_entry():
+    database.initialize_database("test_file/test_database.db")
+    salt =  secrets.token_bytes(SALT_LENGTH)
+    key = kfd.derive_key("MyPassword1234", salt,
+                         DEFAULT_MEMORY_COST, DEFAULT_TIME_COST,
+                         DEFAULT_PARALLELISM)
+    encrypted_data = encryption.encrypt(key, b'Test data')
+    entry = database.create_entry("github", "georgehebo", encrypted_data["ciphertext"],
+                                  encrypted_data["nonce"], encrypted_data["authentication_tag"],
+                                  "test_file/test_database.db")
+    assert entry
+
+def test_create_entry_data(tmp_path):
+    db_path = tmp_path / "test_database.db"
+    database.initialize_database(str(db_path))
+    salt = secrets.token_bytes(SALT_LENGTH)
+    key = kfd.derive_key("MyPassword1234", salt,
+                         DEFAULT_MEMORY_COST, DEFAULT_TIME_COST,
+                         DEFAULT_PARALLELISM)
+    encrypted_data = encryption.encrypt(key, b'Test data')
+    database.create_entry("github", "georgehebo", encrypted_data["ciphertext"],
+                                  encrypted_data["nonce"], encrypted_data["authentication_tag"],
+                                  str(db_path))
+    connection = database.open_connection(str(db_path))
+    cursor = connection.cursor()
+    query = "SELECT * FROM entries"
+    entry = cursor.execute(query).fetchone()
+    database.close_connection(connection)
+
+    assert entry["created_at"]
+    assert entry["updated_at"]
+    assert entry["location"] == "github"
+    assert entry["username"] == "georgehebo"
+    assert entry["ciphertext"] == encrypted_data["ciphertext"]
+    assert entry["nonce"] == encrypted_data["nonce"]
+    assert entry["authentication_tag"] == encrypted_data["authentication_tag"]
+
+def test_create_entry_invalid_db_path():
+    salt = secrets.token_bytes(SALT_LENGTH)
+    key = kfd.derive_key("MyPassword1234", salt,
+                         DEFAULT_MEMORY_COST, DEFAULT_TIME_COST,
+                         DEFAULT_PARALLELISM)
+    encrypted_data = encryption.encrypt(key, b'Test data')
+    entry = database.create_entry("github", "georgehebo", encrypted_data["ciphertext"],
+                          encrypted_data["nonce"], encrypted_data["authentication_tag"],
+                          "invalid/path/database.db")
+    assert not entry
